0a6182e8bd7e065647448f2da08ecf9d8e7925ad
Dynamic-Code <dynamic2106@gmail.com>
2021-04-17 18:52:08 +0530

    Initail background images added with API

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'weather-app';
+  url='https://api.unsplash.com/search/photos?query=%27rainy,sky%27&client_id=pOEt5wvoFKYJZ9Lk2ci7jWcsO7EIrb4COBQwwIzQ_cc'
+  image='https://source.unsplash.com/1920x1080/?rain';
+  //image=''
+  constructor(private http:HttpClient){
+
+  }
+  ngOnInit(){
+    console.log(this.image);
+    
+  //    this.http.get(this.url)
+  //    .subscribe((data:any)=>{
+  //      console.log(data.results[0].urls.full);
+  //      this.image = data.results[4].urls.full;
+  //    })
+    }
 }
+
